5d3e262af57eb05584bc22bd59b7a7766418e6dc
Kristo Kiis <kristo@efley.ee>
2013-01-24 23:10:18 +0200

    mimifying failed, new version, final

--- assets/js/database.js
+++ assets/js/database.js
@@ -1,1 +1,86 @@
-function queryUserId(tx){tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id unique, firstname, lastname, bank, fb_id, mail, sex, accepted, done, rejected, money, prizes)');tx.executeSql('SELECT * FROM USER WHERE id=:id',[user.id],querySuccess,errorDB)}function insertUser(tx){tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id unique, firstname, lastname, bank, fb_id, mail, sex, accepted, done, rejected, money, prizes)');sql='INSERT INTO USER (id, firstname, lastname, fb_id, mail, sex, accepted, done, rejected, money, prizes) VALUES ("'+user.id+'", "'+user.firstname+'", "'+user.lastname+'", "'+user.fb_id+'", "'+user.mail+'", "'+user.sex+'", "0", "0", "0", "0", "0")';tx.executeSql(sql)}function updateUser(tx){sql='UPDATE USER SET firstname = "'+user.firstname+'", lastname = "'+user.lastname+'", fb_id = "'+user.fb_id+'", mail = "'+user.mail+'", sex = "'+user.sex+'", bank = "'+user.bank+'", accepted = "'+user.accepted+'", done = "'+user.done+'", rejected = "'+user.rejected+'", money = "'+user.money+'", prizes = "'+user.prizes+'" WHERE id=:id';tx.executeSql(sql,[user.id])}function updateCustomUser(tx,query){tx.executeSql('UPDATE USER SET '+query+' WHERE id=:id',[user.id])}function queryTasks(tx){tx.executeSql('CREATE TABLE IF NOT EXISTS TASKS (id unique, name, description, status)')}function insertTask(tx,task){sql='INSERT INTO TASKS (id, name, description, status) VALUES ("'+task.id+'", "'+task.name+'", "'+task.description+'", "beginning")';tx.executeSql(sql)}function updateTask(tx,task){tx.executeSql('UPDATE TASKS SET status = "'+task.status+'" WHERE id=:id',[task.id])}function removeTask(tx,id){tx.executeSql('REMOVE FROM TASKS WHERE id=:id',[id])}function successUser(){app.preLoadHome()}function querySuccess(tx,results){if(results.rows.length){var len=results.rows.length;for(var i=0;i<len;i++){if(user.id==results.rows.item(i).id){user.firstname=results.rows.item(i).firstname;user.lastname=results.rows.item(i).lastname;user.fb_id=results.rows.item(i).fb_id;user.mail=results.rows.item(i).mail;user.sex=results.rows.item(i).sex;user.bank=results.rows.item(i).bank;user.accepted=results.rows.item(i).accepted;user.done=results.rows.item(i).done;user.rejected=results.rows.item(i).rejected;user.money=results.rows.item(i).money;user.prizes=results.rows.item(i).prizes}}app.preLoadHome()}else{db.transaction(insertUser,errorDB,successUser);app.preLoadHome()}}function errorDB(tx,err){console.error("Error processing SQL: "+err)}
+//Web SQL stuff
+function queryUserId(tx) {
+	//console.log('user:' + user.id);
+	tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id unique, firstname, lastname, bank, fb_id, mail, sex, accepted, done, rejected, money, prizes)');
+    tx.executeSql('SELECT * FROM USER WHERE id=:id', [user.id], querySuccess, errorDB);
+}
+
+function insertUser(tx) {
+    //tx.executeSql('DROP TABLE IF EXISTS USER');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS USER (id unique, firstname, lastname, bank, fb_id, mail, sex, accepted, done, rejected, money, prizes)');
+	sql = 'INSERT INTO USER (id, firstname, lastname, fb_id, mail, sex, accepted, done, rejected, money, prizes) VALUES ("' + user.id + '", "' + user.firstname + '", "' + user.lastname + '", "' + user.fb_id + '", "' + user.mail + '", "' + user.sex + '", "0", "0", "0", "0", "0")';
+
+	tx.executeSql(sql);
+}
+
+function updateUser(tx) {
+	sql = 'UPDATE USER SET firstname = "' + user.firstname + '", lastname = "' + user.lastname + '", fb_id = "' + user.fb_id + '", mail = "' + user.mail + '", sex = "' + user.sex + '", bank = "' + user.bank + '", accepted = "' + user.accepted + '", done = "' + user.done + '", rejected = "' + user.rejected + '", money = "' + user.money + '", prizes = "' + user.prizes + '" WHERE id=:id';
+	
+	//console.log(sql);
+	
+	tx.executeSql(sql, [user.id]);
+}
+
+function updateCustomUser(tx, query) {
+	
+	tx.executeSql('UPDATE USER SET ' + query + ' WHERE id=:id', [user.id]);
+}
+
+function queryTasks(tx) {
+	tx.executeSql('CREATE TABLE IF NOT EXISTS TASKS (id unique, name, description, status)');
+}
+
+function insertTask(tx, task) {
+	sql = 'INSERT INTO TASKS (id, name, description, status) VALUES ("' + task.id + '", "' + task.name + '", "' + task.description + '", "beginning")';
+
+	tx.executeSql(sql);
+}
+
+function updateTask(tx, task) {
+	
+	tx.executeSql('UPDATE TASKS SET status = "' + task.status + '" WHERE id=:id', [task.id]);
+}
+
+function removeTask(tx, id) {
+	tx.executeSql('REMOVE FROM TASKS WHERE id=:id', [id]);
+}
+
+
+function successUser() {
+	//app.navigate('home.html', 'loadHome');
+	app.preLoadHome();
+}
+
+function querySuccess(tx, results) {
+	if(results.rows.length) {
+		var len = results.rows.length;
+    	for (var i=0; i<len; i++){
+    		if(user.id == results.rows.item(i).id) { 
+				user.firstname = results.rows.item(i).firstname;
+				user.lastname = results.rows.item(i).lastname;
+				user.fb_id = results.rows.item(i).fb_id;
+				user.mail = results.rows.item(i).mail; 
+				user.sex = results.rows.item(i).sex;
+				user.bank = results.rows.item(i).bank;
+				
+				user.accepted = results.rows.item(i).accepted;
+				user.done = results.rows.item(i).done;
+				user.rejected = results.rows.item(i).rejected;
+				user.money = results.rows.item(i).money;
+				user.prizes = results.rows.item(i).prizes;
+			}
+		}
+		app.preLoadHome();
+		//alert('ok');
+	} else {
+	
+		//console.log('No rows :)');
+	
+		db.transaction(insertUser, errorDB, successUser);
+		//app.login();
+		app.preLoadHome();
+	}
+}
+function errorDB(tx, err) {
+    console.error("Error processing SQL: "+err);
+}
--- assets/js/facebook.js
+++ assets/js/facebook.js
@@ -1,1 +1,152 @@
-var my_client_id="405225646214042",my_secret="7c278bfde3f2f806e8cdf44a3f561844",my_redirect_uri="https://www.facebook.com/connect/login_success.html",my_type="user_agent",my_display="touch";var facebook_token="fbToken";var client_browser;var Facebook={init:function(){var authorize_url="https://graph.facebook.com/oauth/authorize?";authorize_url+="client_id="+my_client_id;authorize_url+="&redirect_uri="+my_redirect_uri;authorize_url+="&display="+my_display;authorize_url+="&scope=publish_stream,offline_access"window.plugins.childBrowser.onLocationChange=function(loc){Facebook.facebookLocChanged(loc,false,false)};window.plugins.childBrowser.showWebPage(authorize_url,{showLocationBar:false})},facebookLocChanged:function(loc,post,params){if(loc.indexOf("https://www.facebook.com/connect/login_success.html")==0){var fbCode2=loc.replace(/^.*?\=/,'');fbCode2=fbCode2.replace("#_=_","");token_url='https://graph.facebook.com/oauth/access_token?client_id='+my_client_id+'&client_secret='+my_secret+'&code='+fbCode2+'&redirect_uri=https://www.facebook.com/connect/login_success.html';$.ajax({url:token_url,data:{},dataType:'text',type:'POST',success:function(data,status){window.plugins.childBrowser.close();localStorage.setItem(facebook_token,data.split("=")[1]);if(post){var url='https://graph.facebook.com/me/feed?access_token='+localStorage.getItem(facebook_token);for(var key in params){if(key=="message"){url=url+"&"+key+"="+escape(params[key])}else{url=url+"&"+key+"="+encodeURIComponent(params[key])}}var req=Facebook.request(url)}else{app.login(true)}},error:function(error){app.deliverError('FB get data error on url: '+token_url,'68');window.plugins.childBrowser.close()}})}},request:function(url){var req=new XMLHttpRequest();req.open("POST",url,true);req.send(null);return req},post:function(_fbType,params){if(!localStorage.getItem(facebook_token)){var authorize_url="https://graph.facebook.com/oauth/authorize?";authorize_url+="client_id="+my_client_id;authorize_url+="&redirect_uri="+my_redirect_uri;authorize_url+="&display="+my_display;authorize_url+="&scope=publish_stream,offline_access"window.plugins.childBrowser.onLocationChange=function(loc){Facebook.facebookLocChanged(loc,true,params)};window.plugins.childBrowser.showWebPage(authorize_url,{showLocationBar:false})}else{var url='https://graph.facebook.com/me/'+_fbType+'?access_token='+localStorage.getItem(facebook_token);for(var key in params){if(key=="message"){url=url+"&message=Lahendasin%20just%20%C3%BClesande!"}else{url=url+"&"+key+"="+encodeURIComponent(params[key])}}var req=Facebook.request(url)}},get:function(){data={};$.getJSON('https://graph.facebook.com/me/?access_token='+localStorage.getItem(facebook_token),function(result){data.fb_id=result.id;data.firstname=result.first_name;data.lastname=result.last_name;data.sex=result.gender;data.mail='';app.doLogin(data.fb_id,data)})},success:function(){}};function getQueryVariable(url,variable){var query=url.substring(1);var vars=query.split('&');for(var i=0;i<vars.length;i++){var pair=vars[i].split('=');if(decodeURIComponent(pair[0])==variable){return decodeURIComponent(pair[1])}}}
+// GLOBAL VARS
+var my_client_id = "405225646214042", // YOUR APP ID
+	my_secret = "7c278bfde3f2f806e8cdf44a3f561844", // YOUR APP SECRET 
+	my_redirect_uri = "https://www.facebook.com/connect/login_success.html", // LEAVE THIS
+	my_type ="user_agent", my_display = "touch"; // LEAVE THIS
+ 
+var facebook_token = "fbToken"; // OUR TOKEN KEEPER
+var client_browser;
+
+// FACEBOOK
+var Facebook = {
+	init:function(){
+
+		var authorize_url = "https://graph.facebook.com/oauth/authorize?";
+		 authorize_url += "client_id=" + my_client_id;
+		 authorize_url += "&redirect_uri=" + my_redirect_uri;
+		 authorize_url += "&display=" + my_display;
+		 authorize_url += "&scope=publish_stream,offline_access"
+		 
+		 // Open Child browser and ask for permissions
+		 window.plugins.childBrowser.onLocationChange = function(loc){
+		 	//console.log(loc);
+			Facebook.facebookLocChanged(loc, false, false);
+		 };
+	
+		window.plugins.childBrowser.showWebPage(authorize_url, { showLocationBar: false });
+
+	},
+	facebookLocChanged:function(loc, post, params){
+		
+		if (loc.indexOf("https://www.facebook.com/connect/login_success.html") == 0) {
+
+			var fbCode2 = loc.replace(/^.*?\=/, '');
+			
+			fbCode2 = fbCode2.replace("#_=_","");
+			
+			token_url = 'https://graph.facebook.com/oauth/access_token?client_id='+my_client_id+'&client_secret='+my_secret+'&code='+fbCode2+'&redirect_uri=https://www.facebook.com/connect/login_success.html';
+			
+			$.ajax({
+				url:token_url,
+				data: {},
+				dataType: 'text',
+				type: 'POST',
+				success: function(data, status){
+					//console.log('success');
+					window.plugins.childBrowser.close();
+					// We store our token in a localStorage Item called facebook_token
+					localStorage.setItem(facebook_token, data.split("=")[1]);
+					if (post) {
+						var url = 'https://graph.facebook.com/me/feed?access_token='+localStorage.getItem(facebook_token);
+						for(var key in params){
+							if(key == "message"){
+								url = url+"&"+key+"="+escape(params[key]);
+							}
+							else {
+								url = url+"&"+key+"="+encodeURIComponent(params[key]);
+							}
+						}
+						var req = Facebook.request(url);
+					} else {
+						app.login(true);	
+					}
+					
+				},
+				error: function(error) {
+					app.deliverError('FB get data error on url: ' + token_url, '68');
+					window.plugins.childBrowser.close();
+				}
+			});
+		}
+	},
+	request:function(url){
+		
+		// Create our request and open the connection
+		var req = new XMLHttpRequest(); 
+		req.open("POST", url, true);
+		
+		req.send(null); 
+		return req;
+	},
+	post:function(_fbType,params){
+		
+		if(!localStorage.getItem(facebook_token)) {
+			
+			var authorize_url = "https://graph.facebook.com/oauth/authorize?";
+			authorize_url += "client_id=" + my_client_id;
+			authorize_url += "&redirect_uri=" + my_redirect_uri;
+			authorize_url += "&display=" + my_display;
+			authorize_url += "&scope=publish_stream,offline_access"
+			 
+			// Open Child browser and ask for permissions
+			window.plugins.childBrowser.onLocationChange = function(loc){
+			 	//console.log(loc);
+				Facebook.facebookLocChanged(loc, true, params);
+			};
+		
+			window.plugins.childBrowser.showWebPage(authorize_url, { showLocationBar: false });
+			
+		} else {
+			
+			//console.log(params);
+			
+			var url = 'https://graph.facebook.com/me/'+_fbType+'?access_token='+localStorage.getItem(facebook_token);
+			for(var key in params){
+				if(key == "message"){
+					url = url+"&message=Lahendasin%20just%20%C3%BClesande!";
+				}
+				else {
+					url = url+"&"+key+"="+encodeURIComponent(params[key]);
+				}
+			}
+			var req = Facebook.request(url);
+			
+		}
+
+ 
+	},
+	get: function() {
+		data = {};
+		
+		$.getJSON('https://graph.facebook.com/me/?access_token=' + localStorage.getItem(facebook_token), function(result) {
+
+			data.fb_id = result.id;
+			data.firstname = result.first_name;
+			data.lastname = result.last_name;
+			data.sex = result.gender;
+			data.mail = '';
+			app.doLogin(data.fb_id, data);
+		
+		});
+		
+		//var req = Facebook.request('https://graph.facebook.com/me/?access_token=' + localStorage.getItem(facebook_token));
+		//return req;
+	},
+	success:function(){
+ 
+		//console.log("DONE!");
+ 
+	}
+};
+
+function getQueryVariable(url, variable) {
+    var query = url.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    //console.log('Query variable %s not found', variable);
+}
